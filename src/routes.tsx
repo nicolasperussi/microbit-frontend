6a46d6893126ee0f50ffd3f4c81bc305d7a43265
Nicolas Perussi <nicolasperussi12@hotmail.com>
2022-02-13 20:33:28 -0300

    fixed api endpoint

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,10 +3,8 @@ import {
   BrowserRouter,
   Routes as Switch,
   Route,
-  useLocation,
   Navigate,
 } from "react-router-dom";
-import Navigation from "./components/Navigation";
 import Authentication from "./pages/Authentication";
 import Home from "./pages/Home";
 import Reports from "./pages/Reports";
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const DEV_ENDPOINT = "http://localhost:443/api/v1";
-// const PROD_ENDPOINT = "https://scriptburger.herokuapp.com/api/v1";
-
+const environments = {
+  development: "http://localhost:443/api/v1",
+  production: "https://schoolmicrobit.herokuapp.com/api/v1",
+};
 export const api = axios.create({
-  baseURL: DEV_ENDPOINT,
+  // @ts-ignore
+  baseURL: environments[process.env.REACT_APP_VERCEL_ENV || "development"],
 });
